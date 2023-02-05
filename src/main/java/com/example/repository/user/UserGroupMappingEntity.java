d13cee19397448ee2021086060124398184aee06
bang-star <bang77060986@gmail.com>
2023-02-05 22:49:27 +0900

    #7 - Batch 이용권 일괄 지급
    1. Tasklet 기반 Step을 이용하여 이용권 일괄 지급 Batch를 구현하였다.
    
    2. 필요한 데이터 UserGroupMappingEntity와 BulkPassEntity를 추가하였고
    UserGroupMappingEntity는 기본 키가 2개로 이루어졌기 떄문에 이를 위해 UserGroupMappingId 클래스를 생성하여 구성하였다.
    
    3. Taslket 기반 Step에 내용을 클래스로 분리하여 AddPassesTasklet 클래스로 구성하였다.
    
    4.  이용권 일괄 지급 Batch 기능을 검증하기 위해 Mock을 이용하여 비즈니스 로직에 대한 테스트를 검증하였다.

---
+++ src/main/java/com/example/repository/user/UserGroupMappingEntity.java
@@ -0,0 +1,23 @@
+package com.example.repository.user;
+
+import com.example.repository.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "user_group_mapping")
+@IdClass(UserGroupMappingId.class)
+public class UserGroupMappingEntity extends BaseEntity {
+
+    @Id private String userGroupId;
+    @Id private String userId;
+
+    private String userGroupName;
+    private String description;
+}
