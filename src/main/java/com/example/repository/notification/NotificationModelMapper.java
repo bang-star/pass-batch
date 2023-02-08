9a79c6658461b4015c7b271297bf145012460a24
bang-star <bang77060986@gmail.com>
2023-02-08 18:51:22 +0900

    #9 - mapstruct을 이용한 매핑
    *  mapstruct을 이용하여 BookingEntity정보와 NotificationEvnet을 정보를 기반으로 NotificationEntity로 매핑
    * 전달한 알람 메시지를 생성하기 위해 @Named를 사용하여 원하는 메시지가 생성되도록 함.

---
+++ src/main/java/com/example/repository/notification/NotificationModelMapper.java
@@ -0,0 +1,27 @@
+package com.example.repository.notification;
+
+import com.example.repository.booking.BookingEntity;
+import com.example.util.LocalDateTimeUtils;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+import java.time.LocalDateTime;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface NotificationModelMapper {
+
+    NotificationModelMapper INSTANCE = Mappers.getMapper(NotificationModelMapper.class);
+
+    // 필드명이 같지 않거나 custom하게 매핑해주기 위해서는 @Mapping을 추가해주면 된다.
+    @Mapping(target = "uuid", source = "bookingEntity.userEntity.uuid")
+    @Mapping(target = "text", source = "bookingEntity.startedAt", qualifiedByName = "text")
+    NotificationEntity toNotificationEntity(BookingEntity bookingEntity, NotificationEvent event);
+
+    @Named("test")
+    default String text(LocalDateTime startedAt) {
+        return String.format("안녕하세요. %s 수업 시작합니다. 수업 전 출석 체크 부탁드립니다. \uD83D\uDE0A", LocalDateTimeUtils.format(startedAt));
+    }
+}
