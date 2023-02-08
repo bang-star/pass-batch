86beeac111fd91f907399d6963237994338cb80f
bang-star <bang77060986@gmail.com>
2023-02-08 19:08:21 +0900

    #9 - 날짜 포맷팅
    날짜를 포맷팅하여 전달하기 위한 Util

---
+++ src/main/java/com/example/util/LocalDateTimeUtils.java
@@ -0,0 +1,28 @@
+package com.example.util;
+
+import com.vladmihalcea.hibernate.util.StringUtils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeUtils {
+    public static final DateTimeFormatter YYYY_MM_DD_HH_MM = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public static String format(final LocalDateTime localDateTime) {
+        return localDateTime.format(YYYY_MM_DD_HH_MM);
+
+    }
+
+    public static String format(final LocalDateTime localDateTime, DateTimeFormatter formatter) {
+        return localDateTime.format(formatter);
+
+    }
+
+    public static LocalDateTime parse(final String localDateTimeString) {
+        if(StringUtils.isBlank(localDateTimeString)) {
+            return null;
+        }
+
+        return LocalDateTime.parse(localDateTimeString, YYYY_MM_DD_HH_MM);
+    }
+}
